96cdce15454102c9f891c9ff8d29b20ad6a38be4
赵治鲁 <zhaozhilu@wepiao.com>
2017-06-29 14:16:03 +0800

    习题

---
+++ ex3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+  float fahr, celsius;
+  float lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  fahr = lower;
+  printf("摄氏温度 华氏温度\n");
+  while (fahr <= upper) {
+    celsius = (5.0/9.0)*(fahr-32);
+    printf("%3.0f %6.2f\n", fahr, celsius);
+    fahr = fahr+ step;
+  }
+  return 0;
+}
