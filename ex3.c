ae879f23ecbab44d53aa4b117acdec03cf9a7e20
赵治鲁 <zhaozhilu@wepiao.com>
2017-06-29 14:41:15 +0800

    习题

--- ex3.c
+++ ex3.c
@@ -9,7 +9,7 @@ int main() {
   step = 20;
 
   fahr = lower;
-  printf("摄氏温度 华氏温度\n");
+  printf("华氏温度  摄氏温度\n");
   while (fahr <= upper) {
     celsius = (5.0/9.0)*(fahr-32);
     printf("%3.0f %6.2f\n", fahr, celsius);
---
+++ ex4.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+   float fahr, celsius;
+   float lower, upper, step;
+
+   lower = 0;
+   upper = 300;
+   step = 20;
+
+   celsius = lower;
+   while (celsius <= upper) {
+      fahr = celsius*9.0/5.0 + 32;
+      printf("%3.0f  %6.2f\n", fahr, celsius);
+      celsius = celsius + step;
+   }
+
+   return 0;
+}
