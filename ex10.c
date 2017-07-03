e40675dacad864f971f9711a0bc82fbb112f8416
赵治鲁 <zhaozhilu@wepiao.com>
2017-07-03 10:52:34 +0800

    函数

---
+++ ex10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int power(int m, int n);
+int main() {
+    int i;
+
+    for (i = 1; i < 10; i++) {
+        printf("%d %d %d\n", i, power(2, i), power(-i, 3));
+    }
+    return 0;
+}
+
+int power(int base, int n) {
+    int i, p;
+
+    p = 1;
+    for (i=1; i <= n; ++i) {
+        p = p*base;
+    }
+    return p;
+}
