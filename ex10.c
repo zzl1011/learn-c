70c012dd0fbe5ce3a8d753846747c5c87d74d873
zzl1011 <zhaozhilu1011@163.com>
2017-07-17 09:46:56 +0800

    函数

--- ex10.c
+++ ex10.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
 int power(int m, int n);
-int main() {
+int main() 
+{
     int i;
 
-    for (i = 1; i < 10; i++) {
+    for (i = 1; i < 10; i++) 
+    {
         printf("%d %d %d\n", i, power(2, i), power(-i, 3));
     }
     return 0;
 }
 
-int power(int base, int n) {
+int power(int base, int n) 
+{
     int i, p;
 
     p = 1;
