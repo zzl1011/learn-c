92807637c8c758659cbf93ef748a55c2152d8782
zzl1011 <zhaozhilu1011@163.com>
2017-07-17 09:56:54 +0800

    习题

--- ex9.c
+++ ex9.c
@@ -3,19 +3,24 @@
 #define IN  1  /* inside a word */
 #define OUT 0  /* outside a word */
 
-int main () {
+int main () 
+{
     int c, nl, nw, nc, state;
 
     state = OUT;
     nl = nw = nc = 0;
-    while ((c = getchar()) != EOF) {
+    while ((c = getchar()) != EOF) 
+    {
       ++nc;
-      if (c == '\n') {
+      if (c == '\n') 
+      {
           ++nl;
       }
-      if (c == ' ' || c == '\n' || c == '\t') {
+      if (c == ' ' || c == '\n' || c == '\t') 
+      {
         state = OUT;
-      } else if (state == OUT) {
+      } else if (state == OUT) 
+      {
         state = IN;
         ++nw;
       }
