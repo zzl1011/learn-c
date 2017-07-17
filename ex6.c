1b8556628d43cc6522deb0dffb76ed77d45f99d6
zzl1011 <zhaozhilu1011@163.com>
2017-07-17 09:55:11 +0800

    习题

--- ex6.c
+++ ex6.c
@@ -2,8 +2,10 @@
 
 int fahr;
 
-int main () {
-  for (fahr = 300; fahr > 0; fahr = fahr-20) {
+int main () 
+{
+  for (fahr = 300; fahr > 0; fahr = fahr-20) 
+  {
      printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
   }
   return 0;
